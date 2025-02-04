2801ccfc0f1e6e0b4ccaaf915f676908d6c322c6
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2025-02-04 16:38:22 +0530

    Database connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./config/database');
 
 const app = express();
 
-// Middlewares
+// Connect to database
+connectDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
-
 
 const PORT = process.env.PORT || 5000;
 
